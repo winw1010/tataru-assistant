889b849e30d4d248363718b435ff9fbb8cc52a0c
winw1010 <winw1010@hotmail.com>
2022-04-26 00:15:15 +0800

    Update image-module.js

--- module/image-module.js
+++ module/image-module.js
@@ -228,12 +228,6 @@ function deleteImages() {
     }
 
     try {
-        unlinkSync(getPath('gray.png'));
-    } catch (error) {
-        console.log(error);
-    }
-
-    try {
         unlinkSync(getPath('result.png'));
     } catch (error) {
         console.log(error);

b82f273a1214d2dadf62a215c1701828978019d3
winw1010 <winw1010@hotmail.com>
2022-07-13 15:26:09 +0800

    Fix baidu

--- module/translator/baidu.js
+++ module/translator/baidu.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // baidu
-const baidu = require("baidu-translate-api");
+const baidu = require('translate-baidu')
 
 // translate
 async function translate(text, languageFrom, languageTo) {
@@ -9,7 +9,7 @@ async function translate(text, languageFrom, languageTo) {
         const response = await baidu(text, { from: languageFrom, to: languageTo });
 
         console.log('Baidu:', response);
-        return response.trans_result.dst;
+        return response.dst;
     } catch (error) {
         console.log('Baidu:', error);
         return '';

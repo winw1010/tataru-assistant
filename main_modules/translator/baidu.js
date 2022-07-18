5c64661c858280c0666cd40c13165eee7be2ceb0
winw1010 <winw1010@hotmail.com>
2022-07-18 11:51:23 +0800

    Update baidu.js

--- main_modules/translator/baidu.js
+++ main_modules/translator/baidu.js
@@ -10,7 +10,7 @@ const { signEncoder } = require('./baiduEncoder');
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36';
 
 // RegExp
-const baiduIdRegExp = /(?<target>BAIDUID=.*?);/i;
+const baiduIdRegExp = /(?<target>BAIDUID=.*?)(?=;|$)/i;
 const tokenRegExp = /token:\s*?'(?<target>.*?)'/i;
 const gtkRegExp = /gtk\s*?=\s*?"(?<target>.*?)"/i;
 

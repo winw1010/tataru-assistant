3257c2fbd0744d7b48d85d17acf4f096d786f362
winw1010 <winw1010@hotmail.com>
2022-09-12 08:59:39 +0800

    Fix readme

--- src/config.js
+++ src/config.js
@@ -124,7 +124,7 @@ function setButton() {
     // get google credential
     document.getElementById('a_get_credential').onclick = () => {
         try {
-            exec(`explorer "${fm.getRootPath('src', 'google-api.html')}"`);
+            exec(`explorer "${fm.getRootPath('src', 'readme', 'google-api.html')}"`);
         } catch (error) {
             console.log(error);
         }

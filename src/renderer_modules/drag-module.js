9d3177728b0e107c625d47b83d41030a2be7cef9
winw1010 <winw1010@hotmail.com>
2022-10-03 12:01:33 +0800

    Update drag-module.js

--- src/renderer_modules/drag-module.js
+++ src/renderer_modules/drag-module.js
@@ -1,46 +1,28 @@
 'use strict';
 
-// communicate with main process
+// electron
 const { ipcRenderer } = require('electron');
 
+// set drag element
 function setDragElement(element) {
-    let clientX = 0,
-        clientY = 0,
-        windowWidth = 0,
-        windowHeight = 0;
-
-    element.onmousedown = dragMouseDown;
-
-    function dragMouseDown(e) {
+    element.onmousedown = (e) => {
         e = e || window.event;
         e.preventDefault();
 
-        // get the mouse cursor position at startup:
-        clientX = e.clientX;
-        clientY = e.clientY;
-        windowWidth = window.innerWidth;
-        windowHeight = window.innerHeight;
+        let clientX = e.clientX;
+        let clientY = e.clientY;
+        let windowWidth = window.innerWidth;
+        let windowHeight = window.innerHeight;
 
-        // call a function when mouse button is released:
-        document.onmouseup = closeDragElement;
+        document.onmousemove = () => {
+            ipcRenderer.send('drag-window', clientX, clientY, windowWidth, windowHeight);
+        };
 
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-
-        // set the window's new position:
-        ipcRenderer.send('drag-window', clientX, clientY, windowWidth, windowHeight);
-    }
-
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
+        document.onmouseup = () => {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        };
+    };
 }
 
 // module exports

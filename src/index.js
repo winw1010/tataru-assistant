83483ecde9bf9da440c16d49a7f33f5dcc001fc3
winw1010 <winw1010@hotmail.com>
2022-12-20 10:10:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -303,7 +303,7 @@ async function versionCheck() {
     let notificationText = '';
 
     try {
-        const latestVersion = JSON.parse(await ipcRenderer.invoke('get-latest-version'))?.number;
+        const latestVersion = (await ipcRenderer.invoke('get-latest-version'))?.number;
         const appVersion = ipcRenderer.sendSync('get-version');
 
         if (appVersion === latestVersion) {

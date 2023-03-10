f84ca4c5276077d1e2688014b87d8dab35edd912
winw1010 <winw1010@hotmail.com>
2023-03-10 14:30:50 +0800

    Update translate-module.js

--- src/main_modules/system/translate-module.js
+++ src/main_modules/system/translate-module.js
@@ -95,24 +95,6 @@ async function translate(text, translation, table = []) {
     }
 }
 
-function translate2(text, translation, table = []) {
-    if (text === '') {
-        return '……';
-    }
-
-    const autoChange = translation.autoChange;
-    const languageFrom = translation.from;
-    const languageTo = translation.to;
-    const enginList = engineModule.engineList;
-    let currentEngine = translation.engine;
-    let currentOption = engineModule.getTranslateOption(currentEngine, languageFrom, languageTo, text);
-    let translatedText = '';
-    let previousTranslatedText = '';
-    let tryCount = 0;
-    let codes = table.map((x) => x[0]);
-    let missingCodes = [];
-}
-
 // get translation
 async function getTranslation(engine, option) {
     try {

8645d1ac455e1b4249ed933dc77ec97cbcec66a4
winw1010 <winw1010@hotmail.com>
2023-04-11 09:13:53 +0800

    Update memory-module.js

--- src/main_modules/system/memory-module.js
+++ src/main_modules/system/memory-module.js
@@ -12,43 +12,46 @@ const sharlayanPath = fileModule.getRootPath('src', 'data', 'sharlayan-test', 's
 // bat path
 const batPath = fileModule.getRootPath('src', 'data', 'sharlayan-test', 'start-sharlayan-test.bat');
 
-// bat string
-const batString = `@echo off
-
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\\SysWOW64\\cacls.exe" "%SYSTEMROOT%\\SysWOW64\\config\\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\\system32\\cacls.exe" "%SYSTEMROOT%\\system32\\config\\system"
-)
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\\getadmin.vbs"
-
-    "%temp%\\getadmin.vbs"
-    del "%temp%\\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------    
-"${sharlayanPath}"
-exit`;
-
-function start() {
-    fileModule.fileWriter(batPath, batString);
+// start
+async function start() {
+    fileModule.fileWriter(batPath, createBatString());
     childProcess.execFile(batPath);
+}
+
+// create bat string
+function createBatString() {
+    return `@echo off
+
+    :: BatchGotAdmin
+    :-------------------------------------
+    REM  --> Check for permissions
+        IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+    >nul 2>&1 "%SYSTEMROOT%\\SysWOW64\\cacls.exe" "%SYSTEMROOT%\\SysWOW64\\config\\system"
+    ) ELSE (
+    >nul 2>&1 "%SYSTEMROOT%\\system32\\cacls.exe" "%SYSTEMROOT%\\system32\\config\\system"
+    )
+    
+    REM --> If error flag set, we do not have admin.
+    if '%errorlevel%' NEQ '0' (
+        echo Requesting administrative privileges...
+        goto UACPrompt
+    ) else ( goto gotAdmin )
+    
+    :UACPrompt
+        echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\\getadmin.vbs"
+        set params= %*
+        echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\\getadmin.vbs"
+    
+        "%temp%\\getadmin.vbs"
+        del "%temp%\\getadmin.vbs"
+        exit /B
+    
+    :gotAdmin
+        pushd "%CD%"
+        CD /D "%~dp0"
+    :--------------------------------------    
+    "${sharlayanPath}"
+    exit`;
 }
 
 module.exports = {

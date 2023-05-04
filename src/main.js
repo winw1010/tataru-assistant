3ba1437ed8467a60fa3dbb5399f091b50f18bddd
winw1010 <winw1010@hotmail.com>
2023-05-04 15:06:35 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@
 const { app, BrowserWindow } = require('electron');
 
 // app module
-const appModule = require('./src/module/system/app-module');
+const appModule = require('./module/system/app-module');
 
 // window module
-const windowModule = require('./src/module/system/window-module');
+const windowModule = require('./module/system/window-module');
 
 // when ready
 app.whenReady().then(() => {

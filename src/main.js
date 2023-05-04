207eab1def8a80ba5d4518fb1e6809e52c4bf66f
winw1010 <winw1010@hotmail.com>
2023-05-04 16:50:46 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,18 +9,18 @@ const appModule = require('./module/system/app-module');
 // window module
 const windowModule = require('./module/system/window-module');
 
-// when ready
-app.whenReady().then(() => {
+// on ready
+app.on('ready', () => {
     appModule.startApp();
     windowModule.createWindow('index');
 });
 
 // on window all closed
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
 
 // on activate
-app.on('activate', function () {
+app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) windowModule.createWindow('index');
 });

196adf6d5ca9d2bf34a9a1244b44e5ad6be796b4
winw1010 <winw1010@hotmail.com>
2023-06-06 08:23:32 +0800

    Update index.js

--- src/html/preload/index.js
+++ src/html/preload/index.js
@@ -332,7 +332,7 @@ function resetView(config) {
 // set style
 function setStyle(element, style = {}) {
     Object.keys(style).forEach((key) => {
-        element.style[key] = style[key];
+        if (element?.style?.[key]) element.style[key] = style[key];
     });
 }
 

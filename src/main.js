50fbe061f50687f5ab4058d4022c1260db587c90
winw1010 <winw1010@hotmail.com>
2023-06-09 00:20:23 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 // electron
 const { app, BrowserWindow } = require('electron');
 app.commandLine.appendSwitch('disable-http-cache');
-//app.disableHardwareAcceleration();
 
 // app module
 const appModule = require('./module/system/app-module');

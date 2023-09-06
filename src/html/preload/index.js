461d19fc9a1c8a7f84641616288b21a9deeed2e1
winw1010 <winw1010@hotmail.com>
2023-09-06 09:53:42 +0800

    Update index.js

--- src/html/preload/index.js
+++ src/html/preload/index.js
@@ -284,8 +284,9 @@ function setButton() {
 
 // start app
 function startApp() {
-    ipcRenderer.send('initialize-json');
+    ipcRenderer.send('show-notification', '按下CTRL+F9可查看使用說明書');
     ipcRenderer.send('version-check');
+    ipcRenderer.send('initialize-json');
 }
 
 // reset view

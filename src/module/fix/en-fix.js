d9a0ec28b2b2451c4128a3931db5ff15c9a861b4
winw1010 <winw1010@hotmail.com>
2023-10-02 15:08:43 +0800

    Update en-fix.js

--- src/module/fix/en-fix.js
+++ src/module/fix/en-fix.js
@@ -125,9 +125,6 @@ async function textFix(name = '', text = '', translation = {}) {
     if (target) {
         return fixFunction.replaceText(target[1], chArray.combine, true);
     } else {
-        // special fix
-        text = specialFix(name, text);
-
         // en1
         text = fixFunction.replaceText(text, enArray.en1, true);
 
@@ -137,6 +134,9 @@ async function textFix(name = '', text = '', translation = {}) {
 
         // en2
         text = fixFunction.replaceText(text, enArray.en2, true);
+
+        // special fix
+        text = specialFix(name, text);
 
         // mark fix
         text = fixFunction.markFix(text);

750ff6ca2a637fecc305e63a705c1120f0ca29ae
winw1010 <winw1010@hotmail.com>
2024-03-12 16:43:49 +0800

    Update dialog-module.js

--- src/module/system/dialog-module.js
+++ src/module/system/dialog-module.js
@@ -29,176 +29,203 @@ let hideDialogTimeout = null;
 
 // add dialog
 function addDialog(id, code) {
-    windowModule.sendIndex('add-dialog', {
-        id,
-        code,
-        innerHTML: '<span>...</span>',
-        style: getStyle(code),
-        scroll: false,
-    });
+  windowModule.sendIndex('add-dialog', {
+    id,
+    code,
+    innerHTML: '<span>...</span>',
+    style: getStyle(code),
+    scroll: false,
+  });
 }
 
 // update dialog
 function updateDialog(id, name, text, dialogData = null, scroll = true) {
-    // zh convert
-    if (dialogData?.translation) {
-        name = translateModule.zhConvert(name, dialogData.translation.to);
-        text = translateModule.zhConvert(text, dialogData.translation.to);
-    }
+  // zh convert
+  if (dialogData?.translation) {
+    name = translateModule.zhConvert(name, dialogData.translation.to);
+    text = translateModule.zhConvert(text, dialogData.translation.to);
+  }
 
-    // add dialog
-    windowModule.sendIndex('add-dialog', {
-        id,
-        innerHTML: `<span>${name}</span>${name !== '' ? '：<br />' : ''}<span>${text}</span>`,
-        style: { display: 'block' },
-        scroll: scroll,
-    });
+  // add dialog
+  windowModule.sendIndex('add-dialog', {
+    id,
+    innerHTML: `<span>${name}</span>${
+      name !== '' ? '：<br />' : ''
+    }<span>${text}</span>`,
+    style: { display: 'block' },
+    scroll: scroll,
+  });
 
-    // show dialog
-    showDialog();
+  // show dialog
+  showDialog();
 
-    // save dialog
-    if (dialogData) {
-        saveLog(id, name, text, dialogData);
-    }
+  // save dialog
+  if (dialogData) {
+    saveLog(id, name, text, dialogData);
+  }
 }
 
 // remove dialog
 function removeDialog(id) {
-    windowModule.sendIndex('remove-dialog', id);
+  windowModule.sendIndex('remove-dialog', id);
 }
 
 // show notification
 function showNotification(text) {
-    const config = configModule.getConfig();
-    const timestamp = new Date().getTime();
-    const id = 'sid' + timestamp;
-    const code = 'FFFF';
+  const config = configModule.getConfig();
+  const timestamp = new Date().getTime();
+  const id = 'sid' + timestamp;
+  const code = 'FFFF';
 
-    // zh convert
-    text = translateModule.zhConvert(text, config.translation.to);
+  // zh convert
+  text = translateModule.zhConvert(text, config.translation.to);
 
-    addDialog(id, code);
-    updateDialog(id, '', text);
-    setTimeout(() => {
-        removeDialog(id);
-    }, 5000 + Math.min(text.length * 20, 5000));
+  addDialog(id, code);
+  updateDialog(id, '', text);
+  setTimeout(() => {
+    removeDialog(id);
+  }, 5000 + Math.min(text.length * 20, 5000));
 }
 
 // show dialog
 function showDialog() {
-    clearTimeout(hideDialogTimeout);
-    hideDialogTimeout = null;
+  clearTimeout(hideDialogTimeout);
+  hideDialogTimeout = null;
 
-    const config = configModule.getConfig();
-    windowModule.sendIndex('hide-dialog', false);
+  const config = configModule.getConfig();
+  windowModule.sendIndex('hide-dialog', false);
 
-    if (config.indexWindow.hideDialog) {
-        hideDialogTimeout = setTimeout(() => {
-            windowModule.sendIndex('hide-dialog', true);
-        }, config.indexWindow.hideDialogTimeout * 1000);
-    }
+  if (config.indexWindow.hideDialog) {
+    hideDialogTimeout = setTimeout(() => {
+      windowModule.sendIndex('hide-dialog', true);
+    }, config.indexWindow.hideDialogTimeout * 1000);
+  }
 }
 
 // get style
 function getStyle(code = '0039') {
-    const config = configModule.getConfig();
-    return {
-        fontWeight: config.dialog.weight,
-        color: config.channel[code] || getColor(code),
-        fontSize: config.dialog.fontSize + 'rem',
-        marginTop: config.dialog.spacing + 'rem',
-        borderRadius: config.dialog.radius + 'rem',
-        backgroundColor: config.dialog.backgroundColor,
-    };
+  const config = configModule.getConfig();
+  return {
+    fontWeight: config.dialog.weight,
+    color: config.channel[code] || getColor(code),
+    fontSize: config.dialog.fontSize + 'rem',
+    marginTop: config.dialog.spacing + 'rem',
+    borderRadius: config.dialog.radius + 'rem',
+    backgroundColor: config.dialog.backgroundColor,
+  };
 }
 
 // get color
 function getColor(code) {
-    const chatCode = chatCodeModule.getChatCode();
-    let color = '#FFFFFF';
+  const chatCode = chatCodeModule.getChatCode();
+  let color = '#FFFFFF';
 
-    for (let index = 0; index < chatCode.length; index++) {
-        const element = chatCode[index];
+  for (let index = 0; index < chatCode.length; index++) {
+    const element = chatCode[index];
 
-        if (code === element.ChatCode) {
-            color = element.Color;
-            break;
-        }
+    if (code === element.ChatCode) {
+      color = element.Color;
+      break;
     }
+  }
 
-    return color;
+  return color;
 }
 
 // save dialog
 function saveLog(id, name, text, dialogData) {
-    const item = {
-        id: id,
-        code: dialogData.code,
-        player: dialogData.playerName,
-        name: dialogData.name,
-        text: dialogData.text,
-        audio_text: dialogData.audioText,
-        translated_name: name,
-        translated_text: text,
-        timestamp: dialogData.timestamp,
-        datetime: new Date(dialogData.timestamp).toLocaleString(),
-        translation: dialogData.translation,
-    };
+  const item = {
+    id: id,
+    code: dialogData.code,
+    player: dialogData.playerName,
+    name: dialogData.name,
+    text: dialogData.text,
+    audio_text: dialogData.audioText,
+    translated_name: name,
+    translated_text: text,
+    timestamp: dialogData.timestamp,
+    datetime: new Date(dialogData.timestamp).toLocaleString(),
+    translation: dialogData.translation,
+  };
 
-    const filePath = fileModule.getPath(logLocation, createLogName(item.timestamp));
-    let log = {};
+  const filePath = fileModule.getPath(
+    logLocation,
+    createLogName(item.timestamp)
+  );
+  let log = {};
 
-    // read/create log file
-    if (fileModule.exists(filePath)) {
-        log = fileModule.read(filePath, 'json') || {};
+  // read/create log file
+  if (fileModule.exists(filePath)) {
+    log = fileModule.read(filePath, 'json') || {};
 
-        // fix old bug
-        if (Array.isArray(log)) {
-            log = {};
-        }
+    // fix old bug
+    if (Array.isArray(log)) {
+      log = {};
     }
+  }
 
-    // play audio at first time
-    if (!log[item.id] && npcChannel.includes(dialogData.code) && dialogData.audioText !== '' && dialogData.translation.autoPlay) {
-        const urlList = googleTTS.getAudioUrl(dialogData.audioText, dialogData.translation.from);
-        windowModule.sendIndex('add-audio', urlList);
-    }
+  // play audio at first time
+  if (
+    !log[item.id] &&
+    npcChannel.includes(dialogData.code) &&
+    dialogData.audioText !== '' &&
+    dialogData.translation.autoPlay
+  ) {
+    const urlList = googleTTS.getAudioUrl(
+      dialogData.audioText,
+      dialogData.translation.from
+    );
+    windowModule.sendIndex('add-audio', urlList);
+  }
 
-    // add/replcae log
-    log[item.id] = item;
+  // add/replcae log
+  log[item.id] = item;
 
-    // write log file
-    try {
-        fileModule.write(filePath, log, 'json');
-    } catch (error) {
-        console.error(error);
-    }
+  // write log file
+  try {
+    fileModule.write(filePath, log, 'json');
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // create log name
 function createLogName(milliseconds = null) {
-    const date = Number.isInteger(milliseconds) ? new Date(milliseconds) : new Date();
-    let dateString = date.toLocaleDateString().split('/');
+  const date = Number.isInteger(milliseconds)
+    ? new Date(milliseconds)
+    : new Date();
 
-    if (dateString[1].length < 2) {
-        dateString[1] = '0' + dateString[1];
-    }
+  return (
+    date.getFullYear().toString() +
+    '-' +
+    (date.getMonth() + 1).toString().padStart(2, '0') +
+    '-' +
+    date.getDate().toString().padStart(2, '0') +
+    '.json'
+  );
 
-    if (dateString[2].length < 2) {
-        dateString[2] = '0' + dateString[2];
-    }
+  /*
+  let dateString = date.toLocaleDateString().split('/');
 
-    return dateString.join('-') + '.json';
+  if (dateString[1].length < 2) {
+    dateString[1] = '0' + dateString[1];
+  }
+
+  if (dateString[2].length < 2) {
+    dateString[2] = '0' + dateString[2];
+  }
+
+  return dateString.join('-') + '.json';
+  */
 }
 
 // module exports
 module.exports = {
-    addDialog,
-    updateDialog,
-    removeDialog,
-    showNotification,
-    showDialog,
-    getStyle,
-    createLogName,
+  addDialog,
+  updateDialog,
+  removeDialog,
+  showNotification,
+  showDialog,
+  getStyle,
+  createLogName,
 };

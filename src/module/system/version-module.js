aeb151950602168fe04c55b1136af6b6dda3776d
winw1010 <winw1010@hotmail.com>
2024-03-20 14:38:20 +0800

    Update version-module.js

--- src/module/system/version-module.js
+++ src/module/system/version-module.js
@@ -1,26 +1,30 @@
 'use strict';
 
 function isLatest(appVersion = '', latestVersion = '') {
-  if (appVersion && latestVersion) {
-    const appVersionArray = appVersion.split('.');
-    const latestVersionArray = latestVersion.split('.');
+  try {
+    if (appVersion && latestVersion) {
+      const appVersionArray = appVersion.split('.');
+      const latestVersionArray = latestVersion.split('.');
 
-    if (appVersionArray.length === 3 && latestVersionArray.length === 3) {
-      for (let index = 0; index < 3; index++) {
-        const appVersionNumber = tryParseInt(appVersionArray[index]);
-        const latestVersionNumber = tryParseInt(latestVersionArray[index]);
+      if (appVersionArray.length === 3 && latestVersionArray.length === 3) {
+        for (let index = 0; index < 3; index++) {
+          const appVersionNumber = tryParseInt(appVersionArray[index]);
+          const latestVersionNumber = tryParseInt(latestVersionArray[index]);
 
-        if (appVersionNumber > latestVersionNumber) {
-          return true;
-        } else if (appVersionNumber < latestVersionNumber) {
-          return false;
-        }
+          if (appVersionNumber > latestVersionNumber) {
+            return true;
+          } else if (appVersionNumber < latestVersionNumber) {
+            return false;
+          }
 
-        if (index === 2 && appVersionNumber === latestVersionNumber) {
-          return true;
+          if (index === 2 && appVersionNumber === latestVersionNumber) {
+            return true;
+          }
         }
       }
     }
+  } catch (error) {
+    console.log(error);
   }
 
   return false;

04a8367269e396255fa4629dd8bbac319e0c0624
winw1010 <winw1010@hotmail.com>
2024-03-28 21:07:33 +0800

    Update jp-function.js

--- src/module/fix/jp-function.js
+++ src/module/fix/jp-function.js
@@ -17,8 +17,10 @@ const regKatakanaName =
   /^[の\u3100-\u312F\u3400-\u4DBF\u4E00-\u9FFF]*[ァ-ヺー・＝]+[の？\u3100-\u312F\u3400-\u4DBF\u4E00-\u9FFF]*$/;
 const regKatakanaFront = /^[ァ-ヺー・＝].*[^ァ-ヺー・＝]$/;
 const regKatakanaBack = /^[^ァ-ヺー・＝].*[ァ-ヺー・＝]$/;
-const noKatakanaFront = '(?<![ァ-ヺ])';
-const noKatakanaBack = '(?![ァ-ヺ])';
+const noKatakanaFront =
+  '(?<![アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモヤユヨラリルレロワヰヱヲンヴ])';
+const noKatakanaBack =
+  '(?![アイウエオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモヤユヨラリルレロワヰヱヲンヴ])';
 
 // jp text function
 function replaceTextByCode(text = '', array = [], textType = 0) {

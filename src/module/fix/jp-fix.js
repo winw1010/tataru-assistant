92604eb8b8ab4a1232dca5e52028800e09b5ff2e
winw1010 <winw1010@hotmail.com>
2024-04-11 21:18:12 +0800

    Update jp-fix.js

--- src/module/fix/jp-fix.js
+++ src/module/fix/jp-fix.js
@@ -283,7 +283,7 @@ async function textFixGPT(name = '', text = '', translation = {}) {
   const { gptTable } = jpFunction.replaceTextByCode(text, chArray.combine, textType);
 
   // special fix 2
-  // text = specialFix2(name, text);
+  text = specialFix2(name, text);
 
   // mark fix
   // text = fixFunction.markFix(text);
@@ -398,32 +398,33 @@ function specialFix2(name = '', text = '') {
   let loopCount = 0;
 
   /*
-    // あ、あ(or 漢字)
-    // あ……あ(or 漢字)
-    // あ…あ(or 漢字)
-    loopCount = 0;
-    const RegExpCommaWords1 = /^([…、]*)([ぁ-ゖァ-ヺ][ぁぃぅぇぉゃゅょっァィゥェォャュョッ]*)[…、]+(\2|[一-龠])/gi;
-    const RegExpCommaWords2 = /([、。！？])([…、]*)([ぁ-ゖァ-ヺ][ぁぃぅぇぉゃゅょっァィゥェォャュョッ]*)[…、]+(\3|[一-龠])/gi;
-    while (loopCount < 10) {
-        const result1 = RegExpCommaWords1.test(text);
-        const result2 = RegExpCommaWords2.test(text);
+  // あ、あ(or 漢字)
+  // あ……あ(or 漢字)
+  // あ…あ(or 漢字)
+  loopCount = 0;
+  const RegExpCommaWords1 = /^([…、]*)([ぁ-ゖァ-ヺ][ぁぃぅぇぉゃゅょっァィゥェォャュョッ]*)[…、]+(\2|[一-龠])/gi;
+  const RegExpCommaWords2 = /([、。！？])([…、]*)([ぁ-ゖァ-ヺ][ぁぃぅぇぉゃゅょっァィゥェォャュョッ]*)[…、]+(\3|[一-龠])/gi;
+  while (loopCount < 10) {
+      const result1 = RegExpCommaWords1.test(text);
+      const result2 = RegExpCommaWords2.test(text);
 
-        if (result1) {
-            text = text.replace(RegExpCommaWords1, '$1$3');
-        }
+      if (result1) {
+          text = text.replace(RegExpCommaWords1, '$1$3');
+      }
 
-        if (result2) {
-            text = text.replace(RegExpCommaWords2, '$1$2$4');
-        }
+      if (result2) {
+          text = text.replace(RegExpCommaWords2, '$1$2$4');
+      }
 
-        if (!result1 && !result2) {
-            break;
-        }
+      if (!result1 && !result2) {
+          break;
+      }
 
-        loopCount++;
-    }
-    */
+      loopCount++;
+  }
+  */
 
+  /*
   // あアあ => あああ
   loopCount = 0;
   while (/([^ァ-ヺー・＝]|^)[ァ-ヺ][ァィゥェォャュョッ]?([^ァ-ヺー・＝]|$)/gi.test(text) && loopCount++ < 10) {
@@ -437,6 +438,7 @@ function specialFix2(name = '', text = '') {
       break;
     }
   }
+  */
 
   return text;
 }

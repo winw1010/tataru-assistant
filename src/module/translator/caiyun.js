4a1e6dbb3c36a27214ea0f165a0bd7810723c586
winw1010 <winw1010@hotmail.com>
2024-04-27 13:26:21 +0800

    Update caiyun.js

--- src/module/translator/caiyun.js
+++ src/module/translator/caiyun.js
@@ -1,41 +1,40 @@
 'use strict';
 
+const axios = require('axios').default;
+
 // request module
-const requestModule = require('../system/request-module');
+//const requestModule = require('../system/request-module');
 
 // translate
 async function exec(option) {
   try {
-    const response = await requestModule.post(
+    const response = await axios.post(
+      'https://api.interpreter.caiyunai.com/v1/translator',
       {
-        protocol: 'https:',
-        hostname: 'api.interpreter.caiyunai.com',
-        path: '/v1/translator',
-      },
-      JSON.stringify({
         source: option.text,
         trans_type: `${option.from}2${option.to}`,
         replaced: true,
         detect: true,
         media: 'text',
         request_id: '5a096eec830f7876a48aac47',
-      }),
+      },
       {
-        'Content-Type': 'application/json',
-        'x-authorization': 'token lqkr1tfixq1wa9kmj9po',
+        timeout: 10000,
+        headers: {
+          'Content-Type': 'application/json',
+          'x-authorization': 'token lqkr1tfixq1wa9kmj9po',
+        },
       }
     );
 
-    if (response?.target) {
-      return response.target;
+    if (response.data?.target) {
+      return response.data.target;
     } else {
-      console.log('option:', option);
-      console.log('data:', response);
-      throw 'ERROR: translate';
+      return response.data;
     }
   } catch (error) {
     console.log(error);
-    return '';
+    return error;
   }
 }
 

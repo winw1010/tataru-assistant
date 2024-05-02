c5b50f63419cc717f0d53ede838ed6d9cdecb352
winw1010 <winw1010@hotmail.com>
2024-05-02 10:11:35 +0800

    Update language.js

--- src/html/js/language.js
+++ src/html/js/language.js
@@ -160,11 +160,11 @@ function getElementTextList() {
         'input-dialog-color': ['對話框顏色', '对话框颜色', 'Color'],
         'input-dialog-transparency': ['對話框透明度', '对话框透明度', 'Transparency'],
 
-        'checkbox-auto-change': ['翻譯失敗時切換翻譯引擎', '翻译失败时切换翻译引擎', 'Change engine automatically'],
+        'checkbox-auto-change': ['翻譯失敗時切換翻譯方式', '翻译失败时切换翻译方式', 'Change method automatically'],
         'checkbox-fix-translation': ['翻譯修正', '翻译修正', 'Fix translation'],
         'checkbox-skip-system': ['忽略常見系統訊息', '忽略常见系统讯息', 'Ignore system message'],
         'checkbox-skip-chinese': ['忽略漢化字幕', '忽略汉化字幕', 'Ignore chinese text'],
-        'select-engine': ['翻譯引擎', '翻译引擎', 'Engine'],
+        'select-engine': ['翻譯方式', '翻译方式', 'Method'],
         'select-from': ['遊戲語言', '游戏语言', 'Game lang'],
         'select-from-player': ['隊伍頻道', '队伍频道', 'Party lang'],
         'select-to': ['翻譯語言', '翻译语言', 'Target lang'],

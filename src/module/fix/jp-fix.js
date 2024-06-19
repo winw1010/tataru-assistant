c286c99488b788018e6b8a12218aad3ec5fd7a94
winw1010 <winw1010@hotmail.com>
2024-06-20 03:00:41 +0800

    Update jp-fix.js

--- src/module/fix/jp-fix.js
+++ src/module/fix/jp-fix.js
@@ -147,7 +147,7 @@ async function fixName(dialogData = {}) {
   // skip check
   if (jpFunction.needTranslation(codeResult.text, codeResult.table)) {
     // translate
-    translatedName = await translateModule.translate(codeResult.text, translation, codeResult.table);
+    translatedName = await translateModule.translate(codeResult.text, translation, codeResult.table, 'name');
   }
 
   // table

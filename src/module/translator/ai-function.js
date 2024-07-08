225f14706e90fd93235e9bfefb8adfe343b73bb3
winw1010 <winw1010@hotmail.com>
2024-07-08 08:24:05 +0800

    Update ai-function.js

--- src/module/translator/ai-function.js
+++ src/module/translator/ai-function.js
@@ -1,17 +1,17 @@
 'use strict';
 
 function createSystemContent(source = 'Japanese', target = 'Chinese') {
-  return `You are a professional translator. Your job is translating text from ${source} to ${target} and just return ${target} text to user.`;
+  return `You are a professional translator. Your job is translating the ${source} name or sentence into ${target}. Do not include any explanation.`;
 }
 
 function createPrompt(source = 'Japanese', target = 'Chinese') {
   //I want you to act as an expert translator.
   //let prompt = `You will be provided with a ${type} in ${source}, and your task is to translate it into ${target}. Your response should not be in ${source}.`;
-  return `Translate the following text from ${source} to ${target}. Just return translation.`;
+  return `Translate the following text from ${source} to ${target}. Do not include any explanation.`;
 }
 
 function createImagePrompt() {
-  return 'Copy text from this image. Just return the text.';
+  return 'Copy the text from this image. Do not include any explanation.';
 }
 
 module.exports = {

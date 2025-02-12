b3bf95519d1a0924376b9a4248c4d87baec64458
winw1010 <winw1010@hotmail.com>
2025-02-12 10:05:38 +0800

    Update dialog-module.js

--- src/module/system/dialog-module.js
+++ src/module/system/dialog-module.js
@@ -31,7 +31,7 @@ const logLocation = fileModule.getUserDataPath('log');
 let hideDialogTimeout = null;
 
 // add dialog
-function addDialog(dialogData) {
+function addDialog(dialogData = {}) {
   windowModule.sendIndex('add-dialog', dialogData);
 }
 
@@ -167,7 +167,14 @@ function saveDialog(dialogData) {
 function createLogName(milliseconds = null) {
   const date = Number.isInteger(milliseconds) ? new Date(milliseconds) : new Date();
 
-  return date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0') + '.json';
+  return (
+    date.getFullYear().toString() +
+    '-' +
+    (date.getMonth() + 1).toString().padStart(2, '0') +
+    '-' +
+    date.getDate().toString().padStart(2, '0') +
+    '.json'
+  );
 }
 
 // module exports

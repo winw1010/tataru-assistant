208929d79ef2c74778ce4c37b5c4ba5145f5b815
winw1010 <winw1010@hotmail.com>
2025-02-21 12:09:27 +0800

    Update speech.js

--- src/html/js/speech.js
+++ src/html/js/speech.js
@@ -1,63 +1,81 @@
 'use strict';
 
 {
-  // play list
-  let playlist = [];
-  let playInterval = null;
-  let playSpeed = 1;
-  let isPlaying = false;
-  let nowPlaying = null;
+  // tts object
+  const tts = {
+    list: [],
+    interval: null,
+    speed: 1,
+    enable: false,
+    isPlaying: false,
+    audio: null,
+  };
 
   // add url
   document.addEventListener('add-to-playlist', (event) => {
-    if (isPlaying) playlist = playlist.concat(event.detail);
+    if (tts.enable) tts.list = tts.list.concat(event.detail);
   });
 
   // set speech speed
   document.addEventListener('set-speech-speed', (event) => {
     const speed = parseFloat(event.detail);
-    if (typeof speed === 'number' && !isNaN(speed)) playSpeed = speed;
+    if (typeof speed === 'number' && !isNaN(speed)) tts.speed = speed;
   });
 
   // start playing
   document.addEventListener('start-playing', () => {
-    isPlaying = true;
-
-    clearInterval(playInterval);
-    playInterval = setInterval(() => {
-      if (nowPlaying) return;
-
-      const url = playlist.shift();
-
-      if (url && typeof url === 'string') {
-        nowPlaying = new Audio(url);
-        nowPlaying.currentTime = 0;
-        nowPlaying.volume = 1;
-        nowPlaying.playbackRate = playSpeed;
-
-        nowPlaying.onpause = () => {
-          nowPlaying = null;
-        };
-
-        nowPlaying.onended = () => {
-          nowPlaying = null;
-        };
-
-        nowPlaying.onerror = () => {
-          nowPlaying = null;
-        };
-
-        nowPlaying.play();
-      }
-    }, 1000);
+    tts.enable = true;
+    setPlayInterval();
   });
 
   // stop playing
   document.addEventListener('stop-playing', () => {
-    clearInterval(playInterval);
-    isPlaying = false;
-    nowPlaying?.pause();
-    nowPlaying = null;
-    playlist = [];
+    clearInterval(tts.interval);
+    tts.enable = false;
+    tts.list = [];
+
+    try {
+      tts.audio.pause();
+    } catch (error) {
+      console.log(error);
+    }
   });
+
+  function setPlayInterval() {
+    clearInterval(tts.interval);
+    tts.interval = setInterval(() => {
+      if (tts.isPlaying) return;
+
+      const url = tts.playlist.shift();
+
+      if (url) {
+        try {
+          tts.audio = new Audio(url);
+          tts.audio.currentTime = 0;
+          tts.audio.volume = 1;
+          tts.audio.playbackRate = tts.speed;
+
+          tts.audio.onplay = () => {
+            tts.isPlaying = true;
+          };
+
+          tts.audio.onpause = () => {
+            tts.isPlaying = false;
+          };
+
+          tts.audio.onended = () => {
+            tts.isPlaying = false;
+          };
+
+          tts.audio.onerror = () => {
+            tts.isPlaying = false;
+          };
+
+          tts.audio.play();
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }, 1000);
+  }
 }

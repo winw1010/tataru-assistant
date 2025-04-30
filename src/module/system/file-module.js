e297e791c3be202301f25e0be273e97eb85ce0fa
winw1010 <39563714+winw1010@users.noreply.github.com>
2025-04-30 20:12:37 +0800

    Update file-module.js

--- src/module/system/file-module.js
+++ src/module/system/file-module.js
@@ -27,18 +27,7 @@ const oldName = 'Tataru Helper Node';
 
 // directory check
 function directoryCheck() {
-  if (!fs.existsSync(getUserDataPath()) && fs.existsSync(getOldUserDataPath())) {
-    copyData();
-  }
-
-  const subPath = [
-    '',
-    appName,
-    appName + '\\' + 'config',
-    appName + '\\' + 'image',
-    appName + '\\' + 'log',
-    appName + '\\' + 'text',
-  ];
+  const subPath = ['', appName, appName + '\\' + 'config', appName + '\\' + 'image', appName + '\\' + 'log', appName + '\\' + 'text'];
 
   subPath.forEach((value) => {
     try {
@@ -51,72 +40,6 @@ function directoryCheck() {
       console.log(error);
     }
   });
-}
-
-// copy data
-function copyData() {
-  try {
-    const oldPath = getOldUserDataPath();
-    const newPath = getUserDataPath();
-
-    const renameList = [
-      [getPath(newPath, 'setting'), getPath(newPath, 'config')],
-      [getPath(newPath, 'temp'), getPath(newPath, 'text')],
-      [getPath(newPath, 'text', 'jpTemp.json'), getPath(newPath, 'text', 'custom-source.json')],
-      [getPath(newPath, 'text', 'chTemp.json'), getPath(newPath, 'text', 'custom-target.json')],
-      [getPath(newPath, 'text', 'overwriteTemp.json'), getPath(newPath, 'text', 'custom-overwrite.json')],
-      [getPath(newPath, 'text', 'player.json'), getPath(newPath, 'text', 'player-name.json')],
-    ];
-    const tempList = [];
-
-    const configPath = getPath(newPath, 'config', 'config.json');
-    const configList = ['geminiApiKey', 'cohereToken', 'gptApiKey', 'gptModel'];
-
-    // copy files
-    fs.cpSync(oldPath, newPath, { recursive: true });
-
-    // rename files
-    for (let index = 0; index < renameList.length; index++) {
-      const element = renameList[index];
-
-      if (exists(element[0])) {
-        fs.renameSync(element[0], element[1]);
-      } else {
-        write(element[1], [], 'json');
-      }
-    }
-
-    // delete temp element from custom-target.json
-    const customTarget = read(getPath(newPath, 'text', 'custom-target.json'), 'json') || [];
-    for (let index = customTarget.length - 1; index >= 0; index--) {
-      const element = customTarget[index];
-
-      if (element[2]?.includes('temp')) {
-        element[2] = 0;
-        tempList.push(element);
-        customTarget.splice(index, 1);
-      }
-    }
-
-    // write text files
-    write(getPath(newPath, 'text', 'custom-target.json'), customTarget, 'json');
-    write(getPath(newPath, 'text', 'temp-name.json'), [], 'json');
-
-    // fix config
-    const config = read(configPath, 'json');
-    config.api = {};
-    for (let index = 0; index < configList.length; index++) {
-      const name = configList[index];
-      const value = config?.system?.[name];
-
-      if (typeof value !== 'undefined') {
-        config.api[name] = value;
-      }
-    }
-    write(configPath, config, 'json');
-  } catch (error) {
-    error;
-  }
 }
 
 // readdir
@@ -200,11 +123,7 @@ function write(filePath = './', data = '', type = '') {
         {
           let dataString = JSON.stringify(data).includes('{')
             ? JSON.stringify(data, null, '\t')
-            : JSON.stringify(data)
-                .replaceAll('[[', '[\n\t[')
-                .replaceAll('],', '],\n\t')
-                .replaceAll(']]', ']\n]')
-                .replaceAll('","', '", "');
+            : JSON.stringify(data).replaceAll('[[', '[\n\t[').replaceAll('],', '],\n\t').replaceAll(']]', ']\n]').replaceAll('","', '", "');
           dataString = dataString.replaceAll('\r\n', '\n').replaceAll('\n', '\r\n');
           fs.writeFileSync(filePath, dataString);
         }

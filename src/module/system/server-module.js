a9f8d71cc135bff78e8c58fd7eb4030bc94d1ad3
winw1010 <39563714+winw1010@users.noreply.github.com>
2025-10-07 17:52:19 +0800

    Update server-module.js

--- src/module/system/server-module.js
+++ src/module/system/server-module.js
@@ -45,7 +45,7 @@ function translateData(dialogData) {
 
   // fix new line
   if (config.translation.from === engineModule.languageEnum.ja) {
-    dialogData.text = dialogData.text.replaceAll('[r]', '、').replace(/(?<=[…、。？！])、/gi, '');
+    dialogData.text = dialogData.text.replaceAll('[r]', ' ');
     /*
     if (dialogData.type.includes('CUTSCENE')) {
       dialogData.text = dialogData.text.replaceAll('[r]', '、').replace(/(?<=[…、。？！])、/gi, '');

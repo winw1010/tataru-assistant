72ae289fb979b9b3cad0a33aecfa285bfd46c507
winw1010 <39563714+winw1010@users.noreply.github.com>
2026-05-11 08:09:55 +0800

    Update notification-module.js

--- src/module/system/notification-module.js
+++ src/module/system/notification-module.js
@@ -10,31 +10,31 @@ const message = {
   UPDATE_AVAILABLE: [
     `<span class="text-warning">已有可用的更新，點擊${updateButton}下載最新版本</span>`,
     `<span class="text-warning">已有可用的更新，点击${updateButton}下载最新版本</span>`,
-    `<span class="text-warning">Update available, press ${updateButton} to download the latest version.</span>`,
+    `<span class="text-warning">An update is available. Click ${updateButton} to download the latest version.</span>`,
   ],
-  VERSION_CHECK_ERRORED: ['無法取得版本資訊', '无法取得版本资讯', 'Failed to get version file'],
+  VERSION_CHECK_ERRORED: ['無法取得版本資訊', '无法取得版本资讯', 'Unable to retrieve version information.'],
 
   DOWNLOAD_COMPLETED: ['對照表下載完畢', '对照表下载完毕', 'NO_MESSAGE'],
   LOAD_COMPLETED: ['對照表讀取完畢', '对照表读取完毕', 'NO_MESSAGE'],
-  TEMP_DELETED: ['暫存清除完畢', '暂存清除完毕', 'Temp file deleted'],
+  TEMP_DELETED: ['暫存清除完畢', '暂存清除完毕', 'Cache cleared.'],
 
   SETTINGS_SAVED: ['設定已儲存', '设定已储存', 'Settings saved'],
-  RESTORED_TO_DEFAULT_SETTINGS: ['已恢復預設值', '已恢復预设值', 'Restored to default settings'],
+  RESTORED_TO_DEFAULT_SETTINGS: ['已恢復預設值', '已恢復预设值', 'Restored to default settings.'],
 
-  GOOGLE_CREDENTIAL_SAVED: ['已儲存Google憑證', '已储存Google凭证', 'Google credential saved'],
-  INCORRECT_FILE: ['檔案格式不正確', '档案格式不正确', 'Incorrect file'],
+  GOOGLE_CREDENTIAL_SAVED: ['已儲存Google憑證', '已储存Google凭证', 'Google credential saved.'],
+  INCORRECT_FILE: ['檔案格式不正確', '档案格式不正确', 'Invalid file format.'],
 
-  LENGTH_TOO_SHORT: ['字數不足', '字数不足', 'The length of word is too short'],
-  WORD_SAVED: ['已儲存自訂翻譯', '已储存自订翻译', 'Saved'],
-  WORD_DELETED: ['已刪除自訂翻譯', '已删除自订翻译', 'Deleted'],
+  LENGTH_TOO_SHORT: ['字數不足', '字数不足', 'Insufficient word count.'],
+  WORD_SAVED: ['已儲存自訂翻譯', '已储存自订翻译', 'Custom translation saved.'],
+  WORD_DELETED: ['已刪除自訂翻譯', '已删除自订翻译', 'Custom translation Deleted'],
 
-  CAPTURING_THE_SCREEN: ['正在擷取螢幕畫面', '正在撷取萤幕画面', 'Capturing the screen'],
-  RECOGNIZING_THE_IMAGE: ['正在辨識圖片文字', '正在辨识图片文字', 'Recognizing the image'],
-  RECOGNITION_COMPLETED: ['辨識完成', '辨识完成', 'Recognition completed'],
-  RECOGNITION_EMPTY: ['字串長度為0', '字串长度为0', 'Text is empty string'],
+  CAPTURING_THE_SCREEN: ['正在擷取螢幕畫面', '正在撷取萤幕画面', 'Capturing screenshot...'],
+  RECOGNIZING_THE_IMAGE: ['正在辨識圖片文字', '正在辨识图片文字', 'Recognizing text from image...'],
+  RECOGNITION_COMPLETED: ['辨識完成', '辨识完成', 'Recognition complete.'],
+  RECOGNITION_EMPTY: ['字串長度為0', '字串长度为0', 'Error: Empty string.'],
 
-  FILE_NOT_FOUND: ['檔案不存在', '档案不存在', 'File is not found'],
-  UNABLE_TO_READ_THE_FILE: ['無法讀取檔案', '无法读取档案', 'Unable to read the file'],
+  FILE_NOT_FOUND: ['檔案不存在', '档案不存在', 'File does not exist.'],
+  UNABLE_TO_READ_THE_FILE: ['無法讀取檔案', '无法读取档案', 'Unable to read file.'],
 };
 
 // get message
